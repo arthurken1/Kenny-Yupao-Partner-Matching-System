e3b4d34f6e5b7e38964f557a6298f1f030a7a3b7
Zhang Yuhong <arthurkenf72@hnu.edu.cn>
2025-01-02 16:44:22 +0800

    1.2/2

--- src/main/java/com/yupi/usercenter/job/PreCacheJob.java
+++ src/main/java/com/yupi/usercenter/job/PreCacheJob.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yupi.usercenter.model.domain.User;
 import com.yupi.usercenter.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -27,24 +29,41 @@ public class PreCacheJob {
     private UserService userService;
 
     @Resource
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource
+    private RedissonClient redissonClient;
 
     private List<Long> mainUserList = Arrays.asList(1L);
 
-    @Scheduled(cron = "0 */5 * * * *")
+    @Scheduled(cron = "0 4 0 * * *")
     public void doCacheRecommenduser() {
-        for (Long userId : mainUserList) {
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1,20),queryWrapper);
-            String redisKey = String.format("shayu:user:recommend:%s",userId);
-            ValueOperations valueOperations = redisTemplate.opsForValue();
-            //写缓存,30s过期
-            try {
-                valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
-            } catch (Exception e){
-                log.error("redis set key error",e);
+        RLock lock = redissonClient.getLock("yupao:precachejob:docache:lock");
+        try {
+            if(lock.tryLock(0, -1, TimeUnit.MILLISECONDS)) {
+                System.out.println("这个线程拿到锁：" + Thread.currentThread().getName());
+                for (Long userId : mainUserList) {
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1,20),queryWrapper);
+                    String redisKey = String.format("shayu:user:recommend:%s",userId);
+                    ValueOperations valueOperations = redisTemplate.opsForValue();
+                    //写缓存,30s过期
+                    try {
+                        valueOperations.set(redisKey,userPage,30000, TimeUnit.MILLISECONDS);
+                    } catch (Exception e){
+                        log.error("redis set key error",e);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            log.error("doCacheRecommendUser error",e);
+        } finally {
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
+                System.out.println("这个线程释放了锁：" + Thread.currentThread().getName());
             }
         }
+
 
     }
 
--- src/test/java/com/yupi/usercenter/service/RedissonTest.java
+++ src/test/java/com/yupi/usercenter/service/RedissonTest.java
@@ -2,6 +2,7 @@ package com.yupi.usercenter.service;
 
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RList;
+import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +21,11 @@ public class RedissonTest {
         RList<Object> rList = redissonClient.getList("test-list");
         rList.add("test");
         System.out.println(rList.get(0));
+
+
+        RMap<Object, Object> rMap = redissonClient.getMap("test-map");
+        rMap.put("test", "test");
+        System.out.println(rMap.get(0));
 
     }
 }
