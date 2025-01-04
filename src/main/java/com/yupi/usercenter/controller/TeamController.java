fbcd4ab87a9652881bca334287e938f6a2a366e2
Zhang Yuhong <arthurkenf72@hnu.edu.cn>
2025-01-04 17:18:09 +0800

    1.4/完成粗的的增删改查

--- src/main/java/com/yupi/usercenter/controller/TeamController.java
+++ src/main/java/com/yupi/usercenter/controller/TeamController.java
@@ -1,14 +1,22 @@
 package com.yupi.usercenter.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.usercenter.common.BaseResponse;
+import com.yupi.usercenter.common.ErrorCode;
+import com.yupi.usercenter.common.ResultUtils;
+import com.yupi.usercenter.exception.BusinessException;
+import com.yupi.usercenter.model.domain.Team;
+import com.yupi.usercenter.model.dto.TeamQuery;
 import com.yupi.usercenter.service.TeamService;
 import com.yupi.usercenter.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/team")
@@ -21,6 +29,79 @@ public class TeamController {
 
     @Resource
     private TeamService teamService;
+
+    @PostMapping("/add")
+    public BaseResponse<Long> addTeam(@RequestBody Team team){
+        if(team == null){
+            throw  new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean save = teamService.save(team);
+        if(save == false){
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,"插入失败");
+        }
+        return ResultUtils.success(team.getId());
+    }
+
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteTeam(@RequestBody Long id){
+        if(id <= 0){
+            throw  new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean result = teamService.removeById(id);
+        if(!result){
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,"删除失败");
+        }
+        return ResultUtils.success(true);
+    }
+
+    @PostMapping("/update")
+    public BaseResponse<Boolean> updateTeam(@RequestBody Team team){
+        if(team == null){
+            throw  new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean result = teamService.updateById(team);
+        if(!result){
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,"更新失败");
+        }
+        return ResultUtils.success(true);
+    }
+
+    @GetMapping("/get")
+    public BaseResponse<Team> getTeamById(long id){
+        if(id <= 0){
+            throw  new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Team team = teamService.getById(id);
+        if(team == null){
+            throw new BusinessException(ErrorCode.NULL_ERROR);
+        }
+        return ResultUtils.success(team);
+    }
+
+    @GetMapping("/list")
+    public BaseResponse<List<Team>> listTeams(TeamQuery teamQuery){
+        if(teamQuery == null){
+            throw  new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Team team = new Team();
+        BeanUtils.copyProperties(teamQuery,team);
+        QueryWrapper<Team> queryWrapper = new QueryWrapper<>(team);
+        List<Team> teamList = teamService.list(queryWrapper);
+        return ResultUtils.success(teamList);
+    }
+
+    @GetMapping("/list/page")
+    public BaseResponse<Page<Team>> listTeamsByPage(TeamQuery teamQuery){
+        if(teamQuery == null){
+            throw  new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Team team = new Team();
+        BeanUtils.copyProperties(teamQuery,team);
+        Page<Team> page = new Page<>(teamQuery.getPageNum(),teamQuery.getPageSize());
+        QueryWrapper<Team> queryWrapper = new QueryWrapper<>(team);
+        Page<Team> resulePage = teamService.page(page,queryWrapper);
+        return ResultUtils.success(resulePage);
+    }
 
 
 }
